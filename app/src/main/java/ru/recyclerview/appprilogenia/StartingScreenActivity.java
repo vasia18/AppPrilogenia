76a1f96639443e7a115b6a58cea8b35fbc24f8e9
vasia18 <avi26@mail;.ru>
2019-07-23 17:43:41 +0300

    Тестовое приложение 50%

---
+++ app/src/main/java/ru/recyclerview/appprilogenia/StartingScreenActivity.java
@@ -0,0 +1,15 @@
+package ru.recyclerview.appprilogenia;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StartingScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_starting_screen);
+    }
+}
