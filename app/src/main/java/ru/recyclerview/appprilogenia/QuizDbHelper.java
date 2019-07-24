12f39d1660a2aea9e67701183ea5c020ca6d9899
vasia18 <avi26@mail;.ru>
2019-07-24 18:01:00 +0300

    Написал: private void addQuestion(Question question) {

--- app/src/main/java/ru/recyclerview/appprilogenia/QuizDbHelper.java
+++ app/src/main/java/ru/recyclerview/appprilogenia/QuizDbHelper.java
@@ -1,5 +1,6 @@
 package ru.recyclerview.appprilogenia;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -7,6 +8,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class QuizDbHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "MyAwesomeQuiz.db";
     private static final int DATABASE_VERSION = 1;
+
     private SQLiteDatabase db;
 
     public QuizDbHelper(Context context) {
@@ -20,12 +22,35 @@ public class QuizDbHelper extends SQLiteOpenHelper {
         final String SQL_CREATE_QUESTIONS_TABLE = "CREATE_TABLE, " +
                 QuizContract.QuestionTable.TABLE_NAME + "(" +
                 QuizContract.QuestionTable._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
-                QuizContract.QuestionTable.COLUMN_QUESTION + " "
+                QuizContract.QuestionTable.COLUMN_QUESTION + " TEXT, " +
+                QuizContract.QuestionTable.COLUMN_OPTION1 + " TEXT, " +
+                QuizContract.QuestionTable.COLUMN_OPTION2 + " TEXT, " +
+                QuizContract.QuestionTable.COLUMN_POTION3 + " TEXT, " +
+                QuizContract.QuestionTable.COLUMN_ANSWER_NR + " INTEGER" +
+                ")";
 
+        db.execSQL(SQL_CREATE_QUESTIONS_TABLE);
+        fillQuestionsTable();
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + QuizContract.QuestionTable.TABLE_NAME);
+        onCreate(db);
+
+    }
+
+    private void fillQuestionsTable() {
+        Question q1 = new Question("A is correct", "A", "B", )
+
+    }
+
+    private void addQuestion(Question question) {
+        ContentValues cv = new ContentValues();
+        cv.put(QuizContract.QuestionTable.COLUMN_QUESTION, question.getQuestion());
+        cv.put(QuizContract.QuestionTable.COLUMN_OPTION1, question.getOption1());
+        cv.put(QuizContract.QuestionTable.COLUMN_OPTION2, question.getOption2());
+        cv.put(QuizContract.QuestionTable.COLUMN_POTION3, question.getOption3());
 
     }
 }
