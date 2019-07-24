bf889ffb21380616431ae8b51d92c44258cc1d7c
vasia18 <avi26@mail;.ru>
2019-07-24 12:59:24 +0300

    Добавлен фон и лого на activity_starting_screen

--- app/src/main/java/ru/recyclerview/appprilogenia/StartingScreenActivity.java
+++ app/src/main/java/ru/recyclerview/appprilogenia/StartingScreenActivity.java
@@ -1,15 +1,35 @@
 package ru.recyclerview.appprilogenia;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class StartingScreenActivity extends AppCompatActivity {
 
+    Button button_start_quiz;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_starting_screen);
+
+        button_start_quiz = (Button)findViewById(R.id.button_start_quiz);
+        button_start_quiz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startQuiz();
+            }
+        });
     }
+
+    private void startQuiz(){
+        Intent intent = new Intent(StartingScreenActivity.this, QuizActivity.class);
+        startActivity(intent);
+    }
+
+
 }
